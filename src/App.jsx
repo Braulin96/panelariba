cb482be9a92f2d7d394b0b4361371513041f7dc9
Braulin Pires <braulinpires@hotmail.com>
2025-08-09 16:26:29 +0100

    configure tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-200 h-screen w-full flex items-center justify-center'>
+        Bem vindo a panela riba app
+      </div>
+    </>
+  )
+}
+
+export default App
